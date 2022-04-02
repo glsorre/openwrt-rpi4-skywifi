4a8cd8c80ad20a68185f5e682035c8fe66060d2a
Giuseppe Sorrentino <uhpnou@gmail.com>
2022-04-02 22:56:39 +0000

    small changes

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 make -C openwrt-imagebuilder-21.02.2-bcm27xx-bcm2711.Linux-x86_64 image \
        	PROFILE="rpi-4" \
-       	PACKAGES="irqbalance map kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152 kmod-rtl kmod-hwmon-rpi-poe-fan luci-app-sqm luci-ssl luci-app-https-dns-proxy luci-app-ttyd luci-app-ddns" \
+       	PACKAGES="irqbalance map kmod-usb-net-asix-ax88179 kmod-usb-net-rtl8152 kmod-hwmon-rpi-poe-fan luci-app-sqm luci-ssl luci-app-https-dns-proxy luci-app-ttyd luci-app-ddns" \
        	FILES="files" \
        	EXTRA_IMAGE_NAME="pi4-skywifi"
 
